71ea36c1c2216ae2c66ac8d0bd31900f216942d3
xefheh <09092002Xefr@>
2024-10-29 01:32:30 +0500

    #ENVELOPEDEV17
    - создал layout страниц
    - создал загатовки страниц

---
+++ envelope-frontend/src/app/join/page.tsx
@@ -0,0 +1,10 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: 'Envelope Education: Присоединение к курсу'
+}
+
+export default function JoinPage(): JSX.Element {
+    // TODO: сделать страницу с тасками
+    return <h1>ЗДЕСЬ БУДУТ ПРИСОЕДИНЕНИЯ К КУРСАМ, А ПОКА ТУТ БУКОВКИ</h1>;
+}
